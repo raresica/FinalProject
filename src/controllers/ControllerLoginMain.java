057955f9f048060d9e2fc53331d69999ea37e685
raresica <raresonescu@gmail.com>
2020-05-06 13:02:35 +0300

    login works in console

--- src/controllers/ControllerLoginMain.java
+++ src/controllers/ControllerLoginMain.java
@@ -1,6 +1,8 @@
 package controllers;
 
+import DBConnection.DBHandler;
 import com.jfoenix.controls.*;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -10,6 +12,10 @@ import javafx.stage.Stage;
 
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class ControllerLoginMain implements Initializable {
@@ -30,11 +36,13 @@ public class ControllerLoginMain implements Initializable {
     @FXML
     private JFXButton login;
 
-
+    private DBHandler hander;
+    private Connection connection;
+    private PreparedStatement pst;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-
+    hander = new DBHandler();
     }
 
 
@@ -49,6 +57,41 @@ public class ControllerLoginMain implements Initializable {
     }
 
 
+    @FXML
+    void loginAction(ActionEvent event) {
+        connection =hander.getConnection();
+        String q1= "SELECT * FROM login WHERE username=? and password=?";
+
+        try {
+            pst = connection.prepareStatement(q1);
+            pst.setString(1, username.getText());
+            pst.setString(2, password.getText());
+            ResultSet rs= pst.executeQuery();
+            int count=0;
+            while (rs.next()) {
+                count++;
+            }
+            if (count==1){
+                System.out.println("Login Succesfull");
+            }
+            else
+            {
+                System.out.println("Username or Password are is not correct");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
 
 }
 
